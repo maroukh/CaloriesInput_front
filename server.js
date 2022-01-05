54f8e416b1b29691f3e40962bb2ef0ba523d428f
Marouane KHABBARA <marouane.khabbara@visiativ.com>
2022-01-05 16:51:42 +0100

    changing app name in server.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/inputcaloriesfront'));
+app.use(express.static(__dirname + '/dist/CaloriesInput_front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/inputcaloriesfront/index.html'));
+res.sendFile(path.join(__dirname+'/dist/CaloriesInput_front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
