e400ed51bddb49f69b744965d0277783e3da2f39
Marouane KHABBARA <marouane.khabbara@visiativ.com>
2022-01-05 18:02:12 +0100

    changing server.json

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/CaloriesInput_front/index.html'));
 });*/
 
-app.use(express.static(`${__dirname}/inputcaloriesfront/dist/`));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/CaloriesInput_front'));
 
-app.get('*', (req, res) => {
-    //res.json({ message: "Welcome to Calories Counter application." + `${__dirname}/CaloriesInput_front/dist/index.html` });
-    res.sendFile(`${__dirname}/inputcaloriesfront/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-})
+app.get('/*', (req, res) =>
+   res.sendFile('index.html', {
+      root: 'dist/CaloriesInput_front/'
+   }),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
