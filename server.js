d390f7db57a30cbeac88793864f1cfa055e66cde
Marouane KHABBARA <marouane.khabbara@visiativ.com>
2022-01-06 11:49:21 +0100

    changing package.json

--- server.js
+++ server.js
@@ -2,30 +2,25 @@
 const express = require('express');
 const path = require('path');
 
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/CaloriesInput_front'));
+
+// app.get('*', function (req, res) {
+
+//     res.sendFile(path.join(__dirname + '/dist/CaloriesInput_front/index.html'));
+// });
+
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/CaloriesInput_front'));
 
-app.get('*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/CaloriesInput_front/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/CaloriesInput_front/index.html'));
 });
-
-// Serve only the static files form the dist directory
-//app.use(express.static('./dist/CaloriesInput_front'));
-
-//app.use(express.static(`${__dirname}/CaloriesInput_front/dist/`));
-
-// app.get('/*', (req, res) =>
-//    res.sendFile('index.html', {
-//       root: 'dist/CaloriesInput_front/'
-//    }),
-// );
-
-// app.get('*', (req, res) => {
-//     res.sendFile(`${__dirname}/CaloriesInput_front/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
